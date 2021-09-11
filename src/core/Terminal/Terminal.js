4c3ce1ee7b8957d5195e93eb728674a2cdae0442
floooptimism <jackmord445@gmail.com>
2021-09-11 22:22:20 +0800

    added functionality where a program can read for inputs

--- src/core/Terminal/Terminal.js
+++ src/core/Terminal/Terminal.js
@@ -1,26 +1,28 @@
 function Terminal(terminal){
-    this.m_status = null;
+    this.m_mode = null;
     this.m_terminal = terminal;
     this.m_prompt = null;
     this.m_prompt_text = null;
     this.m_input = '';
     this.m_blinker = null;
 
-    this.m_MODES = ['null','main', 'sub'];
+
+    this.m_sub_reading_done = false; // if this is true, it means the program stdin is done reading.
+    this.m_MODES = ['null','main', 'sub']; // null -> none, main -> main terminal, sub -> stdin for programs
 }
 
 // **************************
 // ! getters / setters
 
-Terminal.prototype.getMode = function () {return this.m_status};
+Terminal.prototype.getMode = function () {return this.m_mode};
 Terminal.prototype.getInput = function (){return this.m_input};
 
-// **************************
+// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
 
 
 Terminal.prototype.newPrompt = function (){
-    if(this.m_status != 'main') {
+    if(this.m_mode != 'main') {
         throw new Error("Terminal is toggled off.");
     }
     const prompt = document.createElement('p');
@@ -80,10 +82,13 @@ Terminal.prototype.toggle = function(val){
         throw new Error("Terminal.toggle -> Invalid mode specified.");
         return;
     }
-    this.m_status = val;
+    this.m_mode = val;
 }
 
 Terminal.prototype.submit = function(){
+    if(this.getMode() == 'sub'){ // if program is currently reading for input, stop reading.
+        this.done_reading();
+    }
     this.m_input = this.m_prompt_text.innerText + this.m_blinker.innerText;
     this.m_prompt_text.innerText += this.m_blinker.innerText;
     this.remove_blinker();
@@ -116,5 +121,46 @@ Terminal.prototype.move_blinker_right = function(){
     this.m_prompt_text.innerText +=   this.m_blinker.innerText.slice(0,1);
     this.m_blinker.innerText = this.m_blinker.innerText.slice(1);
 }
+// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+Terminal.prototype.appendToTerminal = function (element){
+    this.m_terminal.appendChild(element);
+}
+
+
+// STDIN for programs
+Terminal.prototype.readline = async function(){
+    this.start_reading(); //set sub input to false, ** start reading input
+    this.toggle('sub'); //set mode to 'sub'
+    let prompt = document.createElement('span');
+    prompt.classList.add('prompt');
+    let prompt_text = document.createElement('span');
+    prompt_text.classList.add('prompt_text');
+    prompt.appendChild(prompt_text);
+    this.m_prompt_text = prompt_text;
+    this.m_prompt = prompt;
+    this.appendToTerminal(this.m_prompt);
+    this.add_blinker();
+    while(true){
+        await new Promise(wait => {
+            setTimeout(wait, 500);
+        })
+        if(!this.sub_is_reading()){
+            break;
+        }
+    }
+}
+
+Terminal.prototype.done_reading = function(){
+    this.m_sub_reading_done = true;
+}
+Terminal.prototype.start_reading = function(){
+    this.m_sub_reading_done = false;
+}
+Terminal.prototype.sub_is_reading = function(){
+    return !this.m_sub_reading_done;
+}
+
+// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
 export default Terminal;
