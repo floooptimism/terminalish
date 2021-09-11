93cb2edba4779bcd25b4df2884b88796653ad8dd
floooptimism <jackmord445@gmail.com>
2021-09-11 22:24:06 +0800

    read for input inside a program

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import CommandRunner from './core/CommandRunner/CommandRunner';
+import CommandTable from './core/CommandTable/CommandTable';
 import parser from './core/Parser/Parser';
 import Term from './core/Terminal/Terminal';
 
@@ -6,26 +7,33 @@ const el_terminal = document.getElementById('terminal');
 
 const Terminal = new Term(el_terminal);
 
+// init Terminal
 Terminal.toggle('main');
 Terminal.newPrompt();
 
+CommandTable.register_command('echo',"ECho",async function (){
+    console.log("fucker");
+    await Terminal.readline();
+    alert(Terminal.getInput());
+})
 
 
 // ! Main block, this is where the action begins
-document.addEventListener('keydown', (e)=>{
+document.addEventListener('keydown', async (e)=>{
     let stat;
     let mode = Terminal.getMode();
-    
+    console.log(mode);
     if(mode == 'main'){
         stat = Terminal.readKey(e);
         if(stat == 'submit'){
             let parsed = parser.parse(Terminal.getInput());
-            CommandRunner.run(parsed);
+            await CommandRunner.run(parsed);
             Terminal.toggle('main');
             Terminal.newPrompt();
         }
     }else if(mode == 'sub'){
-
+        Terminal.readKey(e);
+        console.log("I'm reading"); 
     }
     
     if(mode != 'null'){
